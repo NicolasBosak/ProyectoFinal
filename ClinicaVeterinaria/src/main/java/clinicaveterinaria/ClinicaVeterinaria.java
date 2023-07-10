6f644a33210a60615524e7e16d8fb2ba5826f36b
NicolasBosak <105665553+NicolasBosak@users.noreply.github.com>
2023-07-10 16:25:16 -0500

    Add files via upload

---
+++ ClinicaVeterinaria/src/main/java/clinicaveterinaria/ClinicaVeterinaria.java
@@ -0,0 +1,17 @@
+package com.mycompany.clinicaveterinaria;
+
+import Interfaz.PantallaUsuario;
+
+public class ClinicaVeterinaria {
+    public static void main(String[] args) {
+       
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                
+                PantallaUsuario pantallaU = new PantallaUsuario();
+                pantallaU.setVisible(true);
+            }
+        });
+    }
+}
